d527bbe16dbe4781185aecee83b7187afb6ee09b
Rahul-519 <rgavit519@gmail.com>
2023-03-04 22:16:25 +0530

    google sign in using node.js

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:docs_clone_flutter/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,11 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
       ),
       home: const LoginScreen(),
     );
   }
 }
-
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,22 @@
+import 'package:docs_clone_flutter/repository/auth_repository.dart';
 import 'package:docs_clone_flutter/screens/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
+  void signInWithGoogle(WidgetRef ref){
+    ref.read(AuthRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: () {},
+          onPressed: () => signInWithGoogle(ref),
           icon: Image.asset(
             'assets/images/g-logo-2.png',
             height: 20,
